bb0e77e2d56a0345ddbb2cb2762a3ecf65d73de3
Carlos Eduardo <afonso.cadu@gmail.com>
2024-12-10 23:48:48 +0000

    Create project-modal-types

---
+++ src/app/projects/project-technologies/project-technologies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-technologies',
+  imports: [],
+  templateUrl: './project-technologies.component.html',
+  styleUrl: './project-technologies.component.scss'
+})
+export class ProjectTechnologiesComponent {
+
+}
