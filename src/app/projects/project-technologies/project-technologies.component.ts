2281a326691d77dd4c035d4528dfd96afba94b18
Carlos Eduardo <afonso.cadu@gmail.com>
2024-12-11 00:01:17 +0000

    Create project-title

--- src/app/projects/project-technologies/project-technologies.component.ts
+++ src/app/projects/project-technologies/project-technologies.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { EmployeeModalComponent } from 'src/app/employees/employee-modal/employee-modal.component';
+import { EmployeeModalComponent } from '../../employees/employees-modal/employees-modal.component';
 import { projectModalData } from '../project-modal/project-modal-types';
 
 @Component({
